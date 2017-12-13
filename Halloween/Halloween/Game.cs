387438ec3ebb27f5b05a9d1e4e28b0f02858da11
Firesword12 <33248040+Firesword12@users.noreply.github.com>
2017-12-13 11:33:41 +0200

    miskit

---
+++ Halloween/Halloween/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halloween
+{
+    class Game
+    {
+        static int PlayerHealth = 100;
+        static int PlayerAttack = 20;
+        static string PlayerName;
+
+        static int MonsterHealth = 75;
+        static int MonsterAttack = 10;
+
+        static void Main(string[] args);
+    }
+}
